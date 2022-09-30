f9b4bf2d66459e58371542150d2b135e174de28b
MarkoSimka <markosimonoski8173@gmail.com>
2022-09-30 22:46:30 +0200

    final commit

--- src/App.js
+++ src/App.js
@@ -8,19 +8,22 @@ import Products from "./components/pages/Covid-Test";
 import Calendar from "./components/Calendar";
 import Novosti2 from "./components/Novosti2";
 import Login from "./components/Login";
+import { useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Router>
-        <Navbar />
         <Switch>
-          <Route path="/login" component={Login} />
-          <Route path="/" exact component={Home} />
-          <Route path="/novosti" component={Services} />
-          <Route path="/novosti-rezulat" component={Novosti2} />
-          <Route path="/covid-test" component={Products} />
-          <Route path="/calendar" component={Calendar} />
+          <Navbar>
+            <Route path="/login" component={Login} />
+            <Route path="/" exact component={Home} />
+            <Route path="/novosti" component={Services} />
+            <Route path="/novosti-rezulat" component={Novosti2} />
+            <Route path="/covid-test" component={Products} />
+            <Route path="/calendar" component={Calendar} />
+          </Navbar>
         </Switch>
       </Router>
     </>
